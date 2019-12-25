988b55cdfafa3e229a699180f9d0c73f77e2a41a
hjc22 <549008556@qq.com>
2019-12-25 13:34:15 +0800

    Update WifiPlugin.java

--- android/src/main/java/com/ly/wifi/WifiPlugin.java
+++ android/src/main/java/com/ly/wifi/WifiPlugin.java
@@ -31,9 +31,7 @@ public class WifiPlugin implements MethodCallHandler {
         IntentFilter filter = new IntentFilter();
         filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
         filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
-        registrar
-                .context()
-                .registerReceiver(delegate.networkReceiver,filter);
+        // registrar.context().registerReceiver(delegate.networkReceiver,filter);
 
         channel.setMethodCallHandler(new WifiPlugin(registrar, delegate));
     }
